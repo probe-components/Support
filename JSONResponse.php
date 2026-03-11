f7dea857bf03d6c3dcddcd518c9e87de1e0ce7b2
Patryk <contact@patryknamyslak.pl>
2026-03-11 00:07:50 +0000

    Update JSONResponse.php

--- JSONResponse.php
+++ JSONResponse.php
@@ -11,18 +11,19 @@ class JSONResponse{
 
     public function __toString(): string{
         header('Content-Type: application/json; charset=utf-8');
-        return JSON::encode(array_merge([
+        return JSON::encode([
             "code" => $this->responseCode->value,
             "message" => $this->message,
-        ], $this->body));
+            ...$this->body
+        ]);
     }
     /**
      * Create a JSON Response object.
      * @param HttpResponseCode|HttpErrorResponseCode $responseCode
-     * @param mixed $message
-     * @param mixed $body An array that will be appended after the code and message keys in the JSON response.
+     * @param string $message
+     * @param array $body An array that will be appended after the code and message keys in the JSON response.
      */
-    public function __construct(protected HttpResponseCode|HttpErrorResponseCode $responseCode = HttpResponseCode::OK, protected ?string $message = null, array $body = []){
+    public function __construct(protected HttpResponseCode|HttpErrorResponseCode $responseCode = HttpResponseCode::OK, protected string $message = "no message provided", array $body = []){
         // Remove the code and message key from the body to prevent conflicts with $responseCode and $message
         $this->body = array_remove_keys(array: $body, keys: ["code", "message"]);
     }
